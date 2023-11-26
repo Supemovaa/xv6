46997722bf9f6259b209e25a2da998bf8035f8c9
Supemovaa <Ma_Tianyao@stu.pku.edu.cn>
2023-11-26 17:20:44 +0800

    modified file name

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -93,3 +93,21 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void){
+  int ticks;
+  uint64 handler;
+  argint(0, &ticks);
+  argaddr(1, &handler);
+  struct proc *p = myproc();
+  p->ticks = ticks;
+  p->handler = (void *)handler;
+  return 0;
+}
+
+uint64 sys_sigreturn(void) {
+  struct proc *p = myproc();
+  memmove(p->trapframe, p->timer_trapframe, sizeof(struct trapframe));
+  p->handling = 0;
+  return p->trapframe->a0;
+}
