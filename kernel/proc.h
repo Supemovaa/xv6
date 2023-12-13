60fe9b5d94be570fd777e5bf56d88d761b07fcb2
Supemovaa <Ma_Tianyao@stu.pku.edu.cn>
2023-12-13 14:59:31 +0800

    added descrips

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,6 +105,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 
+  // for alarm
   int ticks;
   void (*handler)();
   int passticks;
